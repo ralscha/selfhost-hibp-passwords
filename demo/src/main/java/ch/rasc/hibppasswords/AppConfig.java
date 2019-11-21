880139d6c5960348c1a9583daaa889d3df818e1b
Ralph Schaer <ralphschaer@gmail.com>
2019-11-21 17:37:41 +0100

    Upgrade libraries

--- demo/src/main/java/ch/rasc/hibppasswords/AppConfig.java
+++ demo/src/main/java/ch/rasc/hibppasswords/AppConfig.java
@@ -5,9 +5,11 @@ import java.io.File;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties("app")
+@Component
 @Validated
 public class AppConfig {
 
